cd79fa8fe4ff2ff8700d56795f81032613104b1a
Timofey <tnmoskalev63@gmail.com>
2022-12-13 12:46:10 +0400

    First commit

---
+++ src/Components/UI/loader.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Loader = () => {
+  return (
+    <div className='loader-wrapper'>
+        <img src="https://icons8.com/preloaders/img/favicons/favicon-194x194.png"  alt=""/>
+    </div>
+  )
+}
